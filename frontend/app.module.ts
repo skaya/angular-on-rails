26c312d42983cdafffac52eb92c7b6f71999b119
Yuliya Kanapatskaya <konopatskaya.yu@gmail.com>
2017-02-28 02:57:31 -0500

    angular files structuring

---
+++ frontend/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule }            from '@angular/platform-browser';
+import { NgModule }                 from '@angular/core';
+import { FormsModule }              from '@angular/forms';
+import { HttpModule }               from '@angular/http';
+
+import { AppComponent }              from './components/app/app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
