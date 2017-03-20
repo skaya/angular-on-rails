e20aeebf916dd5c6352c7ed0ad558438b1b7cca5
Yuliya Kanapatskaya <konopatskaya.yu@gmail.com>
2017-03-20 18:04:30 -0400

    dinamic declaration helper using

--- frontend/app.module.ts
+++ frontend/app.module.ts
@@ -3,8 +3,24 @@ import { NgModule }                 from '@angular/core';
 import { FormsModule }              from '@angular/forms';
 import { HttpModule }               from '@angular/http';
 
-import { Component1 }              from './components/comp1/comp1.component';
-import { Component2 }              from './components/comp2/comp2.component';
+import { dinamicDeclaration }       from './helper.dinamicDeclaration';
+
+// at least one component should be in bootstraped.
+// AppComponent can be it, no metter it does something or not.
+import { AppComponent }             from './components/app/app.component';
+import { Component1 }               from './components/comp1/comp1.component';
+import { Component2 }               from './components/comp2/comp2.component';
+
+// component which have to be declared
+let declareComponents = new dinamicDeclaration([
+    AppComponent,
+    Component1,
+    Component2
+]).listComponents;
+
+// nested components have to be listed here
+let nestedComponents: any[] = [
+];
 
 @NgModule({
   imports: [
@@ -12,8 +28,9 @@ import { Component2 }              from './components/comp2/comp2.component';
     FormsModule,
     HttpModule
   ],
-  declarations: [Component1, Component2],
-  bootstrap: [Component1, Component2]
+  declarations: [...nestedComponents, ...declareComponents],
+  bootstrap: declareComponents,
+  providers: [] //list of services
 })
 
 export class AppModule { }
---
+++ frontend/helper.dinamicDeclaration.ts
@@ -0,0 +1,25 @@
+export class dinamicDeclaration {
+  bootstrapingComponent: any[] = [];
+
+  constructor(components: any[]) {
+    this.listComponents = components;
+  }
+
+  set listComponents(components: any[]) {
+    let reflectOjb = window['Reflect'];
+    for (let component of components) {
+      // getting selector of a component
+      let selector = reflectOjb.getMetadata('annotations', component)[0].selector;
+      if (document.querySelector(selector)) {
+        // the component goes to array for bootstraping
+        // only if the selector is present on the page
+        this.bootstrapingComponent.push(component);
+      }
+    }
+  }
+
+  get listComponents(): any[] {
+    return this.bootstrapingComponent;
+  }
+}
+
